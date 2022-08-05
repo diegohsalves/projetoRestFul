1c914cf94190c0e2c21504c7950590de3fa7fe4a
Diego Alves <diegohsalves@gmail.com>
2022-08-05 19:52:27 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -7,15 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
 
-//indica que essa classe é um recurso web que é implementado por um controlador rest
 @RestController
-
-//nome do recurso
-
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-	//essa annotation indica que este é o método que responde ao tipo get do http do tipo users
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
