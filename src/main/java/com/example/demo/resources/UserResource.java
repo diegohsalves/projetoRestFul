3e56e822259159c1a076409683248a8f2a3485fa
Diego Alves <diegohsalves@gmail.com>
2022-08-07 10:08:38 -0300

    Service layer, component registration

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,20 +1,39 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		
-		User u = new User(1L, "Diego","diegoalves@gmail.com", "34998188488", "123456");
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	//define que a url tambem tera um parametro
+	@GetMapping(value = "/{id}")
+	
+	//essa annotation especifica que essa id sera a mesma que a url tera como parametro
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
